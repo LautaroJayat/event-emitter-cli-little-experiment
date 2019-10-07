75db10f5b4cddda5c5880ac624b56a5add522138
LautaroJayat <lautaro.jayat@gmail.com>
2019-10-07 17:47:53 -0300

    Added some 'process.exit(), some comments, and moved some  functions to an other file

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 #!/usr/bin/env node
+//  Requiring the arguments of the command line
 const cmds = process.argv;
+
+//  Requiring events in order to extend the class
 const event = require('events');
-if (cmds.length < 8) {
-    console.log('\nHey! Its seems that you dont know how to use this wonderful game\n\n Just type "node app [p1] [kind/asshole] [p2] [kind/asshole] [p3] [kind/asshole]" \n\nWhere [p1] [p2] and [p3] are the names of the players and the others fields says something about them');
-    process.exit();
-}
 
-if (cmds[3] !== 'asshole' && cmds[3] !== 'kind') {
-    console.log('hey, you better check out the rules!');
-    console.log('you put ' + cmds[3] + ' where you should put "asshole" or "kind"');
-    process.exit();
-}
-else if (cmds[5] !== 'asshole' && cmds[5] !== 'kind') {
-    console.log('hey, you better check out the rules!');
-    console.log('you put ' + cmds[5] + ' where you should put "asshole" or "kind"');
-    process.exit();
-}
+//  Requiring the functions of the game
+const { play, addEmiters, validate } = require('./fun');
 
-else if (cmds[7] !== 'asshole' && cmds[7] !== 'kind') {
-    console.log('hey, you better check out the rules!');
-    console.log('you put ' + cmds[7] + ' where you should put "asshole" or "kind"');
-    process.exit();
-}
+//  Validating the arguments that where provided
+validate(cmds);
 
-
+//  Its seems that everithing went ok, so now we can do something with the args
 const arg1 = cmds[2];
 const argP1 = cmds[3]
 
@@ -33,7 +21,7 @@ const argP2 = cmds[5];
 const arg3 = cmds[6];
 const argP3 = cmds[7];
 
-
+//  Adding some emiters to the Humans
 class Person extends event {
     constructor(name, personality) {
         super(name, personality);
@@ -51,47 +39,16 @@ class Person extends event {
 }
 
 
-
+//  For now, only 3 people are going to play
 const person1 = new Person(arg1, argP1);
 const person2 = new Person(arg2, argP2);
 const person3 = new Person(arg3, argP3);
-
 const people = [person1, person2, person3];
-
-people.forEach((person) => {
-    person.on('win', () => {
-        console.log("\n" + person.name + ' says: yes! I win! My number was ' + person.number + "\n")
-    });
-    if (person.personality === 'asshole') {
-        person.on('loose', () => {
-            console.log("\n" + person.name + ' says: ooh fuck! I loose. My number was ' + person.number + "\n")
-        })
-    } else if (person.personality === 'kind') {
-        person.on('loose', () => {
-            console.log("\n" + person.name + ' says: well, I guess I loose. My number was ' + person.number + "\n")
-        })
-    }
-});
-
 var winner = "";
 var max = 0;
 
-function play(players, win, num) {
-    console.log("And now! lets begin the Bigger Number Show!! \nThe players are rolling the dices and.....")
-    for (let i = 0; i < players.length; i++) {
-        if (people[i].number > num) {
-            num = people[i].number;
-            win = people[i].name;
-        }
-    }
+// Time to add emitters to every person in the array
+addEmiters(people);
 
-    players.forEach(player => {
-        if (player.name === win) {
-            player.win()
-        } else {
-            player.loose()
-        }
-    });
-}
-
+// Time to Play
 play(people, winner, max);
---
+++ fun.js
@@ -0,0 +1,67 @@
+//  Just a validation function,
+var validate = function (cmds) {
+    if (cmds.length < 8) {
+        console.log('\nHey! Its seems that you dont know how to use this wonderful game\n\n Just type "node app [p1] [kind/asshole] [p2] [kind/asshole] [p3] [kind/asshole]" \n\nWhere [p1] [p2] and [p3] are the names of the players and the others fields says something about them');
+        process.exit();
+    }
+
+    if (cmds[3] !== 'asshole' && cmds[3] !== 'kind') {
+        console.log('hey, you better check out the rules!');
+        console.log('you put ' + cmds[3] + ' where you should put "asshole" or "kind"');
+        process.exit();
+    }
+    else if (cmds[5] !== 'asshole' && cmds[5] !== 'kind') {
+        console.log('hey, you better check out the rules!');
+        console.log('you put ' + cmds[5] + ' where you should put "asshole" or "kind"');
+        process.exit();
+    }
+
+    else if (cmds[7] !== 'asshole' && cmds[7] !== 'kind') {
+        console.log('hey, you better check out the rules!');
+        console.log('you put ' + cmds[7] + ' where you should put "asshole" or "kind"');
+        process.exit();
+    }
+}
+
+//  The rules of the game and a little introduction
+var play = function (players, win, num) {
+    console.log("And now! lets begin the Bigger Number Show!! \nThe players are rolling the dices and.....")
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].number > num) {
+            num = players[i].number;
+            win = players[i].name;
+        }
+    }
+
+    players.forEach(player => {
+        if (player.name === win) {
+            player.win()
+        } else {
+            player.loose()
+        }
+    });
+    process.exit();
+}
+
+//  Adding emitters depending on if they are kind or just assholes
+var addEmiters = function (arr) {
+    arr.forEach((person) => {
+        person.on('win', () => {
+            console.log("\n" + person.name + ' says: yes! I win! My number was ' + person.number + "\n")
+        });
+        if (person.personality === 'asshole') {
+            person.on('loose', () => {
+                console.log("\n" + person.name + ' says: ooh fuck! I loose. My number was ' + person.number + "\n")
+            })
+        } else if (person.personality === 'kind') {
+            person.on('loose', () => {
+                console.log("\n" + person.name + ' says: well, I guess I loose. My number was ' + person.number + "\n")
+            })
+        }
+    });
+}
+
+//  Exporting everithing
+module.exports.play = play;
+module.exports.addEmiters = addEmiters;
+module.exports.validate = validate;
