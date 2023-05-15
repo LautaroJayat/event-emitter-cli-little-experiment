0bb3457de2cbb51dc8b05f87f0289e4ce3a4b6fa
Lautaro Jayat <48366106+LautaroJayat@users.noreply.github.com>
2023-05-15 19:36:12 -0300

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,24 +1,24 @@
 //  Just a validation function,
 var validate = function (cmds) {
     if (cmds.length < 8) {
-        console.log('\nHey! Its seems that you dont know how to use this wonderful game\n\nJust type "node app [p1] [kind/asshole] [p2] [kind/asshole] [p3] [kind/asshole]" \n\nWhere [p1] [p2] and [p3] are the names of the players and the others fields says something about them');
+        console.log('\nHey! Its seems that you dont know how to use this wonderful game\n\nJust type "node app [p1] [kind/super-kind] [p2] [kind/super-kind] [p3] [kind/super-kind]" \n\nWhere [p1] [p2] and [p3] are the names of the players and the others fields says something about them');
         process.exit();
     }
 
-    if (cmds[3] !== 'asshole' && cmds[3] !== 'kind') {
+    if (cmds[3] !== 'super-kind' && cmds[3] !== 'kind') {
         console.log('hey, you better check out the rules!');
-        console.log('you put ' + cmds[3] + ' where you should put "asshole" or "kind"');
+        console.log('you put ' + cmds[3] + ' where you should put "super-kind" or "kind"');
         process.exit();
     }
-    else if (cmds[5] !== 'asshole' && cmds[5] !== 'kind') {
+    else if (cmds[5] !== 'super-kind' && cmds[5] !== 'kind') {
         console.log('hey, you better check out the rules!');
-        console.log('you put ' + cmds[5] + ' where you should put "asshole" or "kind"');
+        console.log('you put ' + cmds[5] + ' where you should put "super-kind" or "kind"');
         process.exit();
     }
 
-    else if (cmds[7] !== 'asshole' && cmds[7] !== 'kind') {
+    else if (cmds[7] !== 'super-kind' && cmds[7] !== 'kind') {
         console.log('hey, you better check out the rules!');
-        console.log('you put ' + cmds[7] + ' where you should put "asshole" or "kind"');
+        console.log('you put ' + cmds[7] + ' where you should put "super-kind" or "kind"');
         process.exit();
     }
 }
@@ -43,13 +43,13 @@ var play = function (players, win, num) {
     process.exit();
 }
 
-//  Adding emitters depending on if they are kind or just assholes
+//  Adding emitters depending on if they are kind or just super-kind
 var addEmiters = function (arr) {
     arr.forEach((person) => {
         person.on('win', () => {
             console.log("\n" + person.name + ' says: yes! I win! My number was ' + person.number + "\n")
         });
-        if (person.personality === 'asshole') {
+        if (person.personality === 'super-kind') {
             person.on('loose', () => {
                 console.log("\n" + person.name + ' says: ooh fuck! I loose. My number was ' + person.number + "\n")
             })
